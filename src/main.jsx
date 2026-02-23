b1957aaecd33448037d7d499620cd40bb030ed41
Aditya_Phuge <adiphuge712@gmail.com>
2026-02-23 20:03:05 +0530

    Brouser roter changes to hashrouter

--- src/main.jsx
+++ src/main.jsx
@@ -1,4 +1,4 @@
-import { BrowserRouter } from 'react-router-dom'
+import { HashRouter  } from 'react-router-dom'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
@@ -6,7 +6,7 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap/dist/js/bootstrap.bundle.min.js';
 
 createRoot(document.getElementById('root')).render(
-  <BrowserRouter>
+  <HashRouter >
     <App />
-  </BrowserRouter>,
+  </HashRouter >,
 )
