377506edea984986b11ae782d0b2bc111af11851
Aditya_Phuge <adiphuge712@gmail.com>
2026-02-28 09:46:47 +0530

    Loding pages added request added

--- src/Component/AthletComponent/ComplainCoach.jsx
+++ src/Component/AthletComponent/ComplainCoach.jsx
@@ -4,17 +4,17 @@ import axios from 'axios';
 import { jwtDecode } from 'jwt-decode';
 import '../AthletComponent/ComplainCoach.css';
 const ComplainCoach = ({ user }) => {
-  const [userdata,setuserdata]=useState(null);
-    useEffect(()=>{
-      const token = localStorage.getItem("token");
-      if (token) {
-        const decoded = jwtDecode(token);
-        // console.log("Decoded data is : ",decoded.user);
-         setuserdata(decoded.user|| decoded);
-      }else{
-         window.location.href = "/login";
-      }
-     },[]);
+  const [userdata, setuserdata] = useState(null);
+  useEffect(() => {
+    const token = localStorage.getItem("token");
+    if (token) {
+      const decoded = jwtDecode(token);
+      // console.log("Decoded data is : ",decoded.user);
+      setuserdata(decoded.user || decoded);
+    } else {
+      window.location.href = "/login";
+    }
+  }, []);
 
   const [complain, setComplain] = useState({
 
@@ -33,54 +33,89 @@ const ComplainCoach = ({ user }) => {
 
     }
   }
+  const [Loding, setLoding] = useState(false);
+  useEffect(() => {
+    const loddata = async () => {
+      if (userdata) {
+        try {
 
-     if (!userdata) return <div>Loading...</div>;
-  return (
-   <>
-  <NavbarOfAth />
-  <div className="feedback-wrapper d-flex justify-content-center align-items-center min-vh-100 bg-light">
-    <div className="feedback-card shadow rounded p-4">
-      <h2 className="text-center text-primary mb-4">Feedback Form</h2>
-      <form onSubmit={fectdata}>
-        {/* Comment textarea */}
-        <div className="mb-3">
-          <label htmlFor="comment" className="form-label">Your Comment</label>
-          <textarea
-            id="comment"
-            name="comment"
-            placeholder="Enter your comment..."
-            value={complain.comment}
-            onChange={(e) => setComplain({ ...complain, comment: e.target.value })}
-            className="form-control"
-            rows={4}
-            required
-          />
-        </div>
+          setLoding(true);
+          // await new Promise(resolve => setTimeout(resolve, 2000));// for checking the loding 
+         
 
-        {/* Difficulty level select */}
-        <div className="mb-4">
-          <label htmlFor="difficultlevel" className="form-label">Difficulty Level</label>
-          <select
-            id="difficultlevel"
-            name="difficultlevel"
-            className="form-select"
-            value={complain.difficultlevel}
-            onChange={(e) => setComplain({ ...complain, difficultlevel: e.target.value })}
-            required
-          >
-            <option value="">Select Difficulty Level</option>
-            <option value="Easy">Easy</option>
-            <option value="Medium">Medium</option>
-            <option value="Hard">Hard</option>
-          </select>
-        </div>
-
-        {/* Submit button */}
-        <button type="submit" className="btn btn-primary w-100 btn-submit">Submit Feedback</button>
-      </form>
+        } catch (error) {
+          // console.log(error);
+          alert("fail");
+        } finally {
+          setLoding(false);
+        }
+      }
+    }
+    loddata();
+  }, [userdata])
+  if (!userdata) return <div className="loader-overlay">
+    <div className="loader-box">
+      <div className="spinner-border" role="status"></div>
+      <p className="loading-text">Loading...</p>
     </div>
-  </div>
-</>
+  </div>;
+  return (
+    <>
+      {
+        Loding && (
+          <div className="loader-overlay">
+            <div className="loader-box">
+              <div className="spinner-border" role="status"></div>
+              <p className="loading-text">Loading...</p>
+            </div>
+          </div>
+        )
+      }
+
+      <NavbarOfAth />
+      <div className="feedback-wrapper d-flex justify-content-center align-items-center min-vh-100 bg-light">
+        <div className="feedback-card shadow rounded p-4">
+          <h2 className="text-center text-primary mb-4">Feedback Form</h2>
+          <form onSubmit={fectdata}>
+            {/* Comment textarea */}
+            <div className="mb-3">
+              <label htmlFor="comment" className="form-label">Your Comment</label>
+              <textarea
+                id="comment"
+                name="comment"
+                placeholder="Enter your comment..."
+                value={complain.comment}
+                onChange={(e) => setComplain({ ...complain, comment: e.target.value })}
+                className="form-control"
+                rows={4}
+                required
+              />
+            </div>
+
+            {/* Difficulty level select */}
+            <div className="mb-4">
+              <label htmlFor="difficultlevel" className="form-label">Difficulty Level</label>
+              <select
+                id="difficultlevel"
+                name="difficultlevel"
+                className="form-select"
+                value={complain.difficultlevel}
+                onChange={(e) => setComplain({ ...complain, difficultlevel: e.target.value })}
+                required
+              >
+                <option value="">Select Difficulty Level</option>
+                <option value="Easy">Easy</option>
+                <option value="Medium">Medium</option>
+                <option value="Hard">Hard</option>
+              </select>
+            </div>
+
+            {/* Submit button */}
+            <button type="submit" className="btn btn-primary w-100 btn-submit">Submit Feedback</button>
+          </form>
+        </div>
+      </div>
+    </>
   )
 }
 
--- src/Component/AthletComponent/TraningSchedule.jsx
+++ src/Component/AthletComponent/TraningSchedule.jsx
@@ -5,20 +5,20 @@ import axios from 'axios';
 import { jwtDecode } from 'jwt-decode';
 import '../AthletComponent/TraningSchedule.css';
 const TraningSchedule = ({ user }) => {
-  const [userdata,setuserdata]=useState(null);
-  useEffect(()=>{
+  const [userdata, setuserdata] = useState(null);
+  useEffect(() => {
     const token = localStorage.getItem("token");
     if (token) {
       const decoded = jwtDecode(token);
       // console.log("Decoded data is : ",decoded.user);
-       setuserdata(decoded.user|| decoded);
-    }else{
-       window.location.href = "/login";
+      setuserdata(decoded.user || decoded);
+    } else {
+      window.location.href = "/login";
     }
-   },[]);
+  }, []);
 
   const [athdata, setAthdata] = useState({
-    
+
     request: ""
   });
   const [workdata, setworkdta] = useState([]);
@@ -31,26 +31,70 @@ const TraningSchedule = ({ user }) => {
       }
       else {
 
-        alert('Coach not assign yet ');
+        // alert('Coach not assign yet ');
       }
     } catch (error) {
       console.log(error);
       if (!userdata) return <div>Loading...</div>;
     }
   }
+  const [Loding, setLoding] = useState(false);
+  const [hasRequested, setHasRequested] = useState(false);
+  // const checkRequestStatus = async () => {
+  //   try {
+  //     const res = await axios.get(
+  //       `${import.meta.env.VITE_API_URL}/viewrequestbyathelet/${userdata.athid}`
+  //     );
 
+  //     setHasRequested(res.data); // expecting true or false
+  //   } catch (error) {
+  //     console.log(error);
+  //   }
+  // };
+  const [showRequestMessage, setShowRequestMessage] = useState(true);
+  const checkRequestStatus = async () => {
+    try {
+      const res = await axios.get(
+        `${import.meta.env.VITE_API_URL}/viewrequestbyathelet/${userdata.athid}`
+      );
+
+      setHasRequested(res.data);
+
+      if (res.data === true) {
+        setShowRequestMessage(true);
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  };
   useEffect(() => {
-   if(userdata){
-    fatchwork();
-   }
+    const loddata = async () => {
+      if (userdata) {
+        try {
+
+          setLoding(true);
+          // await new Promise(resolve => setTimeout(resolve, 2000));// for checking the loding 
+          await fatchwork();
+          await checkRequestStatus();
+
+        } catch (error) {
+          // console.log(error);
+          alert("fail");
+        } finally {
+          setLoding(false);
+        }
+      }
+    }
+    loddata();
   }, [userdata]);
 
 
   const formsubmit = async (e) => {
     e?.preventDefault();
-if (!userdata) return;
+    if (!userdata) return;
     console.log("Sending athdata:", athdata);
-
+    setLoding(true);
+    // await new Promise(resolve => setTimeout(resolve, 2000));// for checking the loding 
     try {
       await axios.post(
         `${import.meta.env.VITE_API_URL}/addrequest/${userdata.athid}`,
@@ -63,76 +107,124 @@ if (!userdata) return;
       );
 
       alert("Request sent successfully");
+      setHasRequested(true);
 
       setAthdata({
-        
+
         request: ""
       });
 
     } catch (error) {
       console.log(error.response?.data);
       alert("Failed to add request");
+    }finally{
+      setLoding(false);
     }
   }
-if (!userdata) return <div>Loading...</div>;
+  if (!userdata) return <div className="loader-overlay">
+    <div className="loader-box">
+      <div className="spinner-border" role="status"></div>
+      <p className="loading-text">Loading...</p>
+    </div>
+  </div>;
   return (
-   <>
-  <NavbarOfAth />
+    <>
+      {
+        Loding && (
+          <div className="loader-overlay">
+            <div className="loader-box">
+              <div className="spinner-border" role="status"></div>
+              <p className="loading-text">Loading...</p>
+            </div>
+          </div>
+        )
+      }
+      <NavbarOfAth />
 
-  <div className="training-container">
-    <h2 className="page-title">Training Schedule</h2>
+      <div className="training-container">
+        <h2 className="page-title">Training Schedule</h2>
 
-    <div className="table-wrapper">
-      <table className="custom-table">
-        <thead>
-          <tr>
-            <th>#</th>
-            <th>Coach</th>
-            <th>Experience</th>
-            <th>Plan</th>
-            <th>Workout</th>
-            <th>Duration</th>
-            <th>Start</th>
-            <th>End</th>
-          </tr>
-        </thead>
-        <tbody>
-          {workdata.map((d, index) => (
-            <tr key={d.workid}>
-              <td>{index + 1}</td>
-              <td>{d.plan.coachid.name}</td>
-              <td>{d.plan.coachid.experience} yrs</td>
-              <td>{d.plan.planname}</td>
-              <td>{d.workname}</td>
-              <td>{d.duration} min</td>
-              <td>{d.plan.startdate}</td>
-              <td>{d.plan.enddate}</td>
-            </tr>
-          ))}
-        </tbody>
-      </table>
-    </div>
-  </div>
-
-  {userdata.coachid == null && (
-    <div className="modal-overlay">
-      <div className="modal-box">
-        <h3>Request Coach</h3>
-        <form onSubmit={formsubmit}>
-          <textarea
-            name="request"
-            value={athdata.request}
-            onChange={(e) =>
-              setAthdata({ ...athdata, request: e.target.value })
-            }
-            placeholder="Write your request to admin..."
-          />
-          <button type="submit">Send Request</button>
-        </form>
+        <div className="table-wrapper">
+          <table className="custom-table">
+            <thead>
+              <tr>
+                <th>#</th>
+                <th>Coach</th>
+                <th>Experience</th>
+                <th>Plan</th>
+                <th>Workout</th>
+                <th>Duration</th>
+                <th>Start</th>
+                <th>End</th>
+              </tr>
+            </thead>
+            <tbody>
+              {workdata.map((d, index) => (
+                <tr key={d.workid}>
+                  <td>{index + 1}</td>
+                  <td>{d.plan.coachid.name}</td>
+                  <td>{d.plan.coachid.experience} yrs</td>
+                  <td>{d.plan.planname}</td>
+                  <td>{d.workname}</td>
+                  <td>{d.duration} min</td>
+                  <td>{d.plan.startdate}</td>
+                  <td>{d.plan.enddate}</td>
+                </tr>
+              ))}
+            </tbody>
+          </table>
+        </div>
       </div>
-    </div>
-  )}
-</>
+      {/* Show message if already requested */}
+      {/* {userdata.coachid == null && hasRequested && (
+        <div className="modal-overlay">
+          <div className="modal-box">
+            <h3>Request Already Sent</h3>
+            <p style={{ marginTop: "10px" }}>
+              You have already sent a request. Please wait for admin approval.
+            </p>
+          </div>
+        </div>
+      )} */}
+      {userdata.coachid == null && hasRequested && showRequestMessage && (
+        <div className="modal-overlay">
+          <div className="modal-box">
+
+            <div className='buttun-close'>
+              <button
+              className="close-btn"
+              onClick={() => setShowRequestMessage(false)}
+            >
+              ✕
+            </button>
+            </div>
+
+            <h3>Request Already Sent</h3>
+            <p className="modal-message">
+              You have already sent a request. Please wait for admin approval.
+            </p>
+          </div>
+        </div>
+      )}
+      {userdata.coachid == null && !hasRequested && (
+        <div className="modal-overlay">
+          <div className="modal-box">
+            <h3>Request Coach</h3>
+            <form onSubmit={formsubmit}>
+              <textarea
+                name="request"
+                value={athdata.request}
+                onChange={(e) =>
+                  setAthdata({ ...athdata, request: e.target.value })
+                }
+                placeholder="Write your request to admin..."
+              />
+              <button type="submit"disabled={Loding} className='w-100' >{Loding?"Processing...":"Send Request"}</button>
+            </form>
+          </div>
+        </div>
+      )}
+    </>
   )
 }
 
